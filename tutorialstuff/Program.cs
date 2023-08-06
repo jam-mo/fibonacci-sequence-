2c9239c711431af69bc5a60bba68278f962aa8ec
Jamie <jamiemolseed@hotmail.co.uk>
2023-08-06 19:39:53 +0100

    Add project files.

---
+++ tutorialstuff/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tutorialstuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var counter = 0;
+            var fibonacciNumbers = new List<int> {0,1};
+            while (counter < 21) {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+                counter++;
+            }
+            foreach (var number in fibonacciNumbers)
+                Console.WriteLine(number);
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
